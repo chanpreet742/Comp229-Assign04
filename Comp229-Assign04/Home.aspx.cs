c81dc2c7700115a38cc29f1934d230681f557cb7
chanp <chanp@LAPTOP-FU3BI9IJ>
2017-12-15 00:44:23 -0500

    Added JSON file.

--- Comp229-Assign04/Home.aspx.cs
+++ Comp229-Assign04/Home.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +13,14 @@ namespace Comp229_Assign04
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            string path = @"C:\Users\Chanp\Source\repos\Comp229-Assign04\Comp229-Assign04\Assign04.json";
+            string data = File.ReadAllText(path);
+            var collection = JsonConvert.DeserializeObject<List<Assign04>>(data);
+            GridView1.DataSource = collection;
+            GridView1.DataBind();
+
+            
 
         }
     }
